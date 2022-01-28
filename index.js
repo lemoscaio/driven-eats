0965a68b3cb4c5b0f7f3238e203ddc7dadf6f5c1
lemoscaio <caio_lemos94@hotmail.com>
2022-01-28 10:14:34 -0300

    Adicionada função que verifica se o pedido está completo

--- index.js
+++ index.js
@@ -1,85 +1,117 @@
-function clicarPrato() { 
+function clicarPrato() {
     const prato1 = document.querySelector(".prato1");
     const prato2 = document.querySelector(".prato2");
     const prato3 = document.querySelector(".prato3");
     prato1.addEventListener("click", () => {
-        if(prato2.classList.contains("selecionado") || prato3.classList.contains("selecionado")) {
-        prato1.classList.toggle("selecionado");
-        prato2.classList.remove("selecionado");
-        prato3.classList.remove("selecionado");
-        } else {prato1.classList.toggle("selecionado");}
+        if (prato2.classList.contains("selecionado") || prato3.classList.contains("selecionado")) {
+            prato1.classList.toggle("selecionado");
+            prato2.classList.remove("selecionado");
+            prato3.classList.remove("selecionado");
+        } else { prato1.classList.toggle("selecionado"); }
+        verificarSePedidoCompleto();
     })
     prato2.addEventListener("click", () => {
-        if(prato1.classList.contains("selecionado") || prato3.classList.contains("selecionado")) {
+        if (prato1.classList.contains("selecionado") || prato3.classList.contains("selecionado")) {
             prato2.classList.toggle("selecionado");
             prato1.classList.remove("selecionado");
             prato3.classList.remove("selecionado");
-            } else {prato2.classList.toggle("selecionado");}
+        } else { prato2.classList.toggle("selecionado"); }
+        verificarSePedidoCompleto();
     })
     prato3.addEventListener("click", () => {
-        if(prato1.classList.contains("selecionado") || prato2.classList.contains("selecionado")) {
+        if (prato1.classList.contains("selecionado") || prato2.classList.contains("selecionado")) {
             prato3.classList.toggle("selecionado");
             prato1.classList.remove("selecionado");
             prato2.classList.remove("selecionado");
-            } else {prato3.classList.toggle("selecionado");}
+        } else { prato3.classList.toggle("selecionado"); }
+        verificarSePedidoCompleto();
     })
-    
+
+
 }
 
-function clicarBebida() { 
+function clicarBebida() {
     const bebida1 = document.querySelector(".bebida1");
     const bebida2 = document.querySelector(".bebida2");
     const bebida3 = document.querySelector(".bebida3");
     bebida1.addEventListener("click", () => {
-        if(bebida2.classList.contains("selecionado") || bebida3.classList.contains("selecionado")) {
-        bebida1.classList.toggle("selecionado");
-        bebida2.classList.remove("selecionado");
-        bebida3.classList.remove("selecionado");
-        } else {bebida1.classList.toggle("selecionado");}
+        if (bebida2.classList.contains("selecionado") || bebida3.classList.contains("selecionado")) {
+            bebida1.classList.toggle("selecionado");
+            bebida2.classList.remove("selecionado");
+            bebida3.classList.remove("selecionado");
+        } else { bebida1.classList.toggle("selecionado"); }
+        verificarSePedidoCompleto();
     })
     bebida2.addEventListener("click", () => {
-        if(bebida1.classList.contains("selecionado") || bebida3.classList.contains("selecionado")) {
+        if (bebida1.classList.contains("selecionado") || bebida3.classList.contains("selecionado")) {
             bebida2.classList.toggle("selecionado");
             bebida1.classList.remove("selecionado");
             bebida3.classList.remove("selecionado");
-            } else {bebida2.classList.toggle("selecionado");}
+        } else { bebida2.classList.toggle("selecionado"); }
+        verificarSePedidoCompleto();
     })
     bebida3.addEventListener("click", () => {
-        if(bebida1.classList.contains("selecionado") || bebida2.classList.contains("selecionado")) {
+        if (bebida1.classList.contains("selecionado") || bebida2.classList.contains("selecionado")) {
             bebida3.classList.toggle("selecionado");
             bebida1.classList.remove("selecionado");
             bebida2.classList.remove("selecionado");
-            } else {bebida3.classList.toggle("selecionado");}
+        } else { bebida3.classList.toggle("selecionado"); }
+        verificarSePedidoCompleto();
     })
-    
+
 }
 
-function clicarSobremesa() { 
+function clicarSobremesa() {
     const sobremesa1 = document.querySelector(".sobremesa1");
     const sobremesa2 = document.querySelector(".sobremesa2");
     const sobremesa3 = document.querySelector(".sobremesa3");
     sobremesa1.addEventListener("click", () => {
-        if(sobremesa2.classList.contains("selecionado") || sobremesa3.classList.contains("selecionado")) {
-        sobremesa1.classList.toggle("selecionado");
-        sobremesa2.classList.remove("selecionado");
-        sobremesa3.classList.remove("selecionado");
-        } else {sobremesa1.classList.toggle("selecionado");}
+        if (sobremesa2.classList.contains("selecionado") || sobremesa3.classList.contains("selecionado")) {
+            sobremesa1.classList.toggle("selecionado");
+            sobremesa2.classList.remove("selecionado");
+            sobremesa3.classList.remove("selecionado");
+        } else { sobremesa1.classList.toggle("selecionado"); }
+        verificarSePedidoCompleto();
     })
     sobremesa2.addEventListener("click", () => {
-        if(sobremesa1.classList.contains("selecionado") || sobremesa3.classList.contains("selecionado")) {
+        if (sobremesa1.classList.contains("selecionado") || sobremesa3.classList.contains("selecionado")) {
             sobremesa2.classList.toggle("selecionado");
             sobremesa1.classList.remove("selecionado");
             sobremesa3.classList.remove("selecionado");
-            } else {sobremesa2.classList.toggle("selecionado");}
+        } else { sobremesa2.classList.toggle("selecionado"); }
+        verificarSePedidoCompleto();
     })
     sobremesa3.addEventListener("click", () => {
-        if(sobremesa1.classList.contains("selecionado") || sobremesa2.classList.contains("selecionado")) {
+        if (sobremesa1.classList.contains("selecionado") || sobremesa2.classList.contains("selecionado")) {
             sobremesa3.classList.toggle("selecionado");
             sobremesa1.classList.remove("selecionado");
             sobremesa2.classList.remove("selecionado");
-            } else {sobremesa3.classList.toggle("selecionado");}
+        } else { sobremesa3.classList.toggle("selecionado"); }
+        verificarSePedidoCompleto();
     })
+}
+
+
+function verificarSePedidoCompleto() {
+    let contador = 0;
+    const pratos = document.querySelectorAll(".prato");
+    pratos.forEach((prato) => {
+        // console.log(prato);
+        if (prato.classList.contains("selecionado")) {
+            // console.log("contém");
+            contador += 1;
+        }
+    });
+    console.log(contador);
+
+    const botaoPedidoCompleto = document.querySelector(".barra-inferior--pedido-completo");
     
+    if (contador === 3) {
+        botaoPedidoCompleto.classList.remove("escondido")
+    } else if (contador < 3) {
+        botaoPedidoCompleto.classList.add("escondido")
+        }
+
 }
 
 clicarPrato();
